ac593c8ddd08875cde106a2ad8d0328fca369928
itssafi <jsafiulla@zeomega.com>
2015-04-17 11:32:41 +0530

    Addind restful services

--- zopper/__init__.py
+++ zopper/__init__.py
@@ -1,11 +1,3 @@
-"""Main entry point
 """
-from pyramid.config import Configurator
-
-
-def main(global_config, **settings):
-    config = Configurator(settings=settings)
-    config.include("cornice")
-    config.scan("bus_booking.views")
-    config.scan("bus_booking.renderer")
-    return config.make_wsgi_app()
+Initialization
+"""
