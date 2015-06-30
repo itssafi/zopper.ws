ec946ed2b88e12b2990fd62b43d1f90851f79a90
itssafi <safiulla.shaikh@accionlabs.com>
2015-06-30 13:14:08 +0530

    adding tests

--- tests/test_zopper_load_and_search_data.py
+++ tests/test_zopper_load_and_search_data.py
@@ -3,6 +3,7 @@ Automated test module
 """
 
 import unittest
+import os
 
 
 class IntegrationTest(unittest.TestCase):
@@ -12,29 +13,39 @@ class IntegrationTest(unittest.TestCase):
         application = get_app('development.ini', 'main')
         from webtest import TestApp
         self.app = TestApp(application)
-        return self.app
 
 
 class TestDataLoad(IntegrationTest):
 
     def setUp(self):
         self.start_simple_wsgi_server()
+        self.data = {"fields":[{"id":"a","label":"IMAGE INTENSIFICATION BASED DEVICES",
+        "type":"string"},{"id":"b","label":None,"type":"string"},{"id":"c","label":None,
+        "type":"string"},{"id":"d","label":None,"type":"string"}],"data":[["DEVICE NAME",
+        "MGNIFICATION (X)","FIELD OF VIEW(degree)","RANGE(m)"],
+        ["Integrated Observation Equipment",7,7,2000],
+        ["Passive Night Vision Binocular (Mk-I)",4,10,500],
+        ["Passive Night Vision Goggles",1,40,225],
+        ["Passive Night Sight for 84 mm Carl",4,10,500],
+        ["Passive Night Sight for 5.56 mm Rifle",4,10,200],
+        ["Passive Night Sight for 5.56 mm LMG",4,10,200],
+        ["Passive Night Sight RCL Mk I",5,10,600],
+        ["Passive Night Sight RCL Mk II",6,9,600],
+        ["Driver Sight for T-55",1,40,100],["Gunners Night Sight for T-55",7,7,">800"],
+        ["Balloon Lifted Imaging & Surveillance","NA",2.1,4000],
+        ["Goggles for Aireforce",1,40,250],["Sight for 84 Mm",5.5,8,700]]}
 
     def test_data_load(self):
         """
         Test loading into database
         """
-        DATA = """{"fields":[{"id":"a","label":"IMAGE INTENSIFICATION BASED DEVICES","type":"string"},{"id":"b","label":None,"type":"string"},{"id":"c","label":None,"type":"string"},{"id":"d","label":None,"type":"string"}],"data":[["DEVICE NAME","MGNIFICATION (X)","FIELD OF VIEW(degree)","RANGE(m)"],["Integrated Observation Equipment",7,7,2000],["Passive Night Vision Binocular (Mk-I)",4,10,500],["Passive Night Vision Goggles",1,40,225],["Passive Night Sight for 84 mm Carl",4,10,500],["Passive Night Sight for 5.56 mm Rifle",4,10,200],["Passive Night Sight for 5.56 mm LMG",4,10,200],["Passive Night Sight RCL Mk I",5,10,600],["Passive Night Sight RCL Mk II",6,9,600],["Driver Sight for T-55",1,40,100],["Gunners Night Sight for T-55",7,7,">800"],["Balloon Lifted Imaging & Surveillance","NA",2.1,4000],["Goggles for Aireforce",1,40,250],["Sight for 84 Mm",5.5,8,700]]}"""
-
         url = 'http://127.0.0.1:8080/dataload/'
-        res = self.app.post(url, DATA, [('content-type','application/json')])
+        headers = [('Content-Type', 'application/json'),
+                   ('Content-Length', str(len(self.data)))]
+        
+        self.app.authorization = ('Basic', ('user', 'password'))
+        res = self.app.post(url, self.data, headers=headers)
         assert res.status_int == 201
-
-
-class TestSearchData(IntegrationTest):
-
-    def setUp(self):
-        self.start_simple_wsgi_server()
 
     def test_search_data(self):
         """
